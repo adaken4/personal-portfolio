73feb617facea6df733fa5699489c9e65bc8d3fc
adaken4 <adakennedy@outlook.com>
2025-10-09 20:26:17 +0300

    feat: integrate skills section into the main page (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <main>
         <Hero />
         <About />
+        <Skills />
       </main>
     </div>
   );
