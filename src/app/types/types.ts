3854361fa637d420846211ccb26c4a75da723933
adaken4 <adakennedy@outlook.com>
2025-10-16 22:18:46 +0300

    feat: add Project interface defining project properties for ProjectCard component (#7)

---
+++ src/app/types/types.ts
@@ -0,0 +1,12 @@
+export interface Project {
+  title: string;
+  tagline: string;
+  description: string;
+  image: string;
+  tech: string[];
+  role: string;
+  challenges: string;
+  results: string;
+  liveUrl: string;
+  githubUrl: string;
+}
