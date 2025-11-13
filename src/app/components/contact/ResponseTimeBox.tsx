4023d4c3d20a393edca2ef33731bd3d9174ac590
adaken4 <adakennedy@outlook.com>
2025-11-13 23:40:25 +0300

    feat: add ResponseTimeBox component to display response time information (#9)

---
+++ src/app/components/contact/ResponseTimeBox.tsx
@@ -0,0 +1,13 @@
+export default function ResponseTimeBox() {
+  return (
+    <div className="rounded-xl border border-blue-200 bg-gradient-to-br from-blue-50 to-cyan-50 p-6 dark:border-gray-600 dark:from-gray-900 dark:to-gray-700">
+      <h4 className="mb-2 font-semibold text-gray-900 dark:text-white">
+        Response Time
+      </h4>
+      <p className="text-gray-700 dark:text-gray-300">
+        I usually respond within 24 hours. Looking forward to connecting with
+        you!
+      </p>
+    </div>
+  );
+}
